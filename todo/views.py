722a30f4ee7f3b23c6f8579a1eaaf36ce5d49b31
KM <tsnok777@gmail.com>
2022-08-03 15:28:13 +0300

    auth user

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,13 @@
 from django.db import IntegrityError
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
+
+def home(request):
+    return render(request, "todo/home.html")
 
 def signupuser(request):
     if request.method == "GET":
@@ -24,6 +27,26 @@ def signupuser(request):
 
 def currenttodos(request):
     return render(request, "todo/currenttodos.html")
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+def loginuser(request):
+    if request.method == "GET":
+        return render(request, "todo/loginuser.html", {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request,username = request.POST['username'] ,password = request.POST['password'])
+        if user is None:
+            return render(request, "todo/loginuser.html", {'form': AuthenticationForm(),'error':'Username and password dodn\'t match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+
+
+
 
 
 
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -21,6 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #Auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
     #Todoes
     path('current/', views.currenttodos, name='currenttodos'),
+    path('', views.home, name='home'),
 ]
