2e353064de65bb15972c027f753f4d75662adde6
KM <tsnok777@gmail.com>
2022-08-06 13:35:40 +0300

    Model todo + dispaly it  in admin panel

--- todo/views.py
+++ todo/views.py
@@ -39,7 +39,7 @@ def loginuser(request):
     else:
         user = authenticate(request,username = request.POST['username'] ,password = request.POST['password'])
         if user is None:
-            return render(request, "todo/loginuser.html", {'form': AuthenticationForm(),'error':'Username and password dodn\'t match'})
+            return render(request, "todo/loginuser.html", {'form': AuthenticationForm(),'error':'Username and password didn\'t match'})
         else:
             login(request, user)
             return redirect('currenttodos')
