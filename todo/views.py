3504e24862f2389ddd66ab805c39dc5546478f23
KM <tsnok777@gmail.com>
2022-08-08 14:39:34 +0300

    create list form elements of todo

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
 from .forms import TodoForm
+from .models import Todo
 
 # Create your views here.
 
@@ -27,7 +28,8 @@ def signupuser(request):
             return render(request, "todo/signupuser.html", {'form': UserCreationForm(),'error':'Password did not match'})
 
 def currenttodos(request):
-    return render(request, "todo/currenttodos.html")
+    todos = Todo.objects.filter(user = request.user, datecompleted__isnull=True)
+    return render(request, "todo/currenttodos.html",{'todos':todos,})
 
 def logoutuser(request):
     if request.method == 'POST':
