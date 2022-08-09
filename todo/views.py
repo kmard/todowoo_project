076579aed9683e7047be6712dc7181d009cfee73
KM <tsnok777@gmail.com>
2022-08-09 14:37:06 +0300

    delete todo

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,13 @@
+
 from django.db import IntegrityError
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
+
 
 # Create your views here.
 
@@ -61,10 +64,31 @@ def  createtodo(request):
             return render(request, "todo/createtodo.html", {'form': TodoForm(),'error':'Bad data passed in, try again'})
 
 
+def viewtodo(request,todo_pk):
+    todo = get_object_or_404(Todo,pk=todo_pk,user = request.user)
+    if request.method == "GET":
+        form = TodoForm(instance=todo)
+        return render(request, "todo/viewtodo.html", {'todo': todo, 'form': form})
+    else:
+        try:
+            form = TodoForm(request.POST,instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, "todo/viewtodo.html", {'form': todo,'form':form, 'error': 'Bad info'})
 
 
+def completetodo(request,todo_pk):
+    todo = get_object_or_404(Todo,pk=todo_pk,user = request.user)
+    if request.method == "POST":
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
 
 
-
-
-
+def deletetodo(request,todo_pk):
+    todo = get_object_or_404(Todo,pk=todo_pk,user = request.user)
+    if request.method == "POST":
+        todo.datecompleted = timezone.now()
+        todo.delete()
+        return redirect('currenttodos')
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -22,9 +22,13 @@ urlpatterns = [
     #Auth
     path('signup/', views.signupuser, name='signupuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
-    path('login/', views.loginuser, name='loginuser'),
+    path('viewtodo/', views.loginuser, name='loginuser'),
     #Todoes
-    path('current/', views.currenttodos, name='currenttodos'),
-    path('create/', views.createtodo, name='createtodo'),
     path('', views.home, name='home'),
+    path('create/', views.createtodo, name='createtodo'),
+    path('current/', views.currenttodos, name='currenttodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+
 ]
