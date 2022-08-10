45a9aece3543295725ad865a7361d534015784ab
KM <tsnok777@gmail.com>
2022-08-10 16:15:10 +0300

    Complited carkas

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -22,11 +22,12 @@ urlpatterns = [
     #Auth
     path('signup/', views.signupuser, name='signupuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
-    path('viewtodo/', views.loginuser, name='loginuser'),
+    path('login/', views.loginuser, name='loginuser'),
     #Todoes
     path('', views.home, name='home'),
     path('create/', views.createtodo, name='createtodo'),
     path('current/', views.currenttodos, name='currenttodos'),
+    path('complited/', views.complitedtodos, name='complitedtodos'),
     path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
     path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
     path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
