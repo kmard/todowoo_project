ef02627f96e77456db6dab550897b79497bd0e0c
KM <tsnok777@gmail.com>
2023-03-02 15:05:25 +0200

    Create, Retrieve, Update, Destroy

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('todos/completed', views.TodoCompletedList.as_view()),
-
-
+    path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
+    path('todos', views.TodoListCreate.as_view()),
 ]
